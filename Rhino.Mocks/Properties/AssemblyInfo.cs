5c1012f41dd58e38d4c15e29ae0fe423f7e88e0f
Greg Sochanik <greg.sochanik@7digital.com>
2011-06-09 10:53:24 +0100

    Added AssembluInfo.cs files back in and removed them from gitignore

---
+++ Rhino.Mocks/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rhino.Mocks")]
+[assembly: AssemblyDescription("Rhino.Mocks")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AC7F98D3-B086-4492-9D5A-8980F61E7851")]
+[assembly: CLSCompliant(true)]
